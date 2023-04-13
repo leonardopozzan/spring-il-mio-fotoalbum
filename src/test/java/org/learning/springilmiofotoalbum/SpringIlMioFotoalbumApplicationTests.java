cbcf3d66898f70759d1e32b2d958c242b1e2de81
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-04-13 11:00:16 +0200

    scaffolding

---
+++ src/test/java/org/learning/springilmiofotoalbum/SpringIlMioFotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.springilmiofotoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIlMioFotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
